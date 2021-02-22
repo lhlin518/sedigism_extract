f5701072979049e76f8844e8978d8f4782b468b1
Liang-Hao Lin <79336653+lhlin518@users.noreply.github.com>
2021-02-22 13:52:52 +0100

    Update extract_region.py
    input, path, download, getdata.

--- extract_region.py
+++ extract_region.py
@@ -8,6 +8,8 @@ if __name__ == '__main__':
 
     #def sedigism_extract():
         # Inputs of function
+        
+        '''
         line = 13   # V0: 13/18, V1: 13/18/'13'/'18'/'13CO'/'C18O' all supported. note the filename in the webpage is lowercase as c18o21
         # fieldcenter = '001'   # should be calculated automatically from the input.
         clon = 355.7  # Galatic longtitude of extracted cube [deg]
@@ -18,7 +20,76 @@ if __name__ == '__main__':
         # Read in datacube
         fitfile = 'G'+fieldcenter+'_'+line+'_Tmb_DR1.fits'
         datacube = fits.open(fitfile)[0]
+        '''
+        line = 13   
+        if line == 13 or line == '13' or line == '13CO':
+            lineFilename = '13CO21'     # the filename in the webpage
+            lineHeader = '13CO J=2-1'   # the name in header['COMMENT'] or somewhere
+            freqLine = 220398.6842 * u.MHz
+        elif line == 18 or line == '18' or line == 'C18O':
+            lineFilename = 'C18O21'     # the filename in the webpage
+            lineHeader = 'C18O J=2-1'   # the name in header['COMMENT'] or somewhere
+            freqLine = 219560.3541 * u.MHz
+        else:
+            sys.exit("Please input line as \'13CO\' or \'C18O\'." )
+        
+        # fieldcenter = '001'   # should be calculated automatically from the input.
+        #clon = 355.7  # Galatic longtitude of extracted cube [deg]
+        #clat = -0.4 # Galatic latitutde of extracted cube [deg]
+        #extSizeL = 900 # half size of the extracted cube [arcsecond] (e.g. +/- 100")
+        
+        # In astronomy, lonLeft > lonRight, latBottom < latTop
+        lonLeft = 355.7     # clon + extSizeL / 2
+        lonRight = 354.9    # clat - extSizeB / 2
+        clon = ( lonLeft + lonRight ) / 2
+        
+        # if the longitude range covers two integer, it is too large.
+        if np.floor( lonLeft ) - np.ceil( lonRight ) > 0:
+            sys.exit('This script cannot extract data from two blocks.')
+        
+        lonBlockCenter = np.round(clon).astype(int)
+        filename = 'G{:d}_'.format(lonBlockCenter) + lineFilename + '_Tmb_DR1.fits'
+        
+        # search the path
+        if inputPath == None:
+            inputPath = './'         # default to current folder
+        elif inputPath == 'webpage' or inputPath == 'Web':
+            inputPath = 'web'
+        elif inputPath[-1] != '/':
+            inputPath = inputPath + '/'   # end with '/'
+        weblink = 'https://sedigism.mpifr-bonn.mpg.de/SEDIGISM_DATACUBES_DR1c/'
+        for path in [ inputPath, './', 'MPIfR', 'web' ]:
+            if path != 'MPIfR' and path != 'web':
+                try:
+                    f = open( path + filename )
+                    f.close()
+                    break
+                except IOError:
+                    print( path + ' does not cotain ' + filename )
+                    continue
+            elif path == 'MPIfR':
+                try:
+                    path = '/apex-archieve/'
+                    f = open( path + filename )
+                    f.close()
+                    break
+                except IOError:
+                    print( path + ' does not cotain ' + filename )
+                    continue
+            elif path == 'web':
+                path == './'
+                with requests.get( weblink + filename, timeout=5, stream=True ) as req:
+                    with open( './' + filename, 'wb' ) as f:   
+                        for chunk in req.iter_content(chunk_size=1024*1024):
+                            if chunk:
+                                f.write( chunk )
+                break
+        # End getdata
+        # open the fits
+        data, header = fits.getdata( path + filename, header=True )
 
+        
+        
         # Convert subcube center from world to pixel coordinate
         cworld = SkyCoord(l=clon,b=clat,unit=(u.deg, u.deg),frame='galactic')
         w = WCS(datacube.header)
